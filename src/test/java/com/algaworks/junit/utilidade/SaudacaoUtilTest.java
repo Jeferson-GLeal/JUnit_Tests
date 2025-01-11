594714b07084e900f75dd069ffba6d13977bd757
Jeferson Leal <jeferson.gleal@outlook.com>
2025-01-11 20:18:48 -0300

    :white_check_mark:[test] Implementado testes Successfull, Shold throw an Exception and Do not Shold throw an Exception

---
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,29 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+    @Test
+    public void saudar() {
+        String saudacao = SaudacaoUtil.saudar(9);
+        assertEquals("Bom dia", saudacao, "Saudacao invalida");
+    }
+
+    @Test
+    public void deveLancarException() {
+
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
+                () -> SaudacaoUtil.saudar(-10));
+
+        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+
+    }
+
+    @Test
+    public void naoDeveLancarException() {
+        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
+    }
+}
