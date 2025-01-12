6cf68c2abb7e3aac2eacd9e2da5aa07026c02b5f
Jeferson Leal <jeferson.gleal@outlook.com>
2025-01-12 17:01:36 -0300

    :white_check_mark:[test] Implementado @DisplayNameGeneration.

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -1,6 +1,8 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -8,9 +10,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
     @Test
-    public void deveRetornarNumerosPares() {
+    void deve_Retornar_Numeros_Pares_() {
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
